91950bf7ad95c4f9e768e1355024443c9e1919ea
TOWHIDpro <sajjad.rk05@gmail.com>
2022-08-06 00:07:43 +0600

    final

--- Index/urls.py
+++ Index/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.create_user, name="create_user"),
     path('test-page', views.index, name="index"),
+    path('user-login', views.usr_login, name="user-login"),
+    path('result', views.result, name="result"),
+    path('all_result', views.all_result, name="all_result"),
    
 ]
--- Index/views.py
+++ Index/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import calculator_func
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout
+from . models import Healthdata
 
 # Create your views here.
 
@@ -30,7 +31,47 @@ def index(request):
             message = 'You are in Mid Risk'
         else:
             message = "You are in Low Risk"
+
+        # Save to database
+        Healthdata.objects.create(
+            user=request.user,
+            age=age,
+            systolicbp=systolicbp,
+            diastolicbp=diastolicbp,
+            bs=bs,
+            heartRate=heartRate,
+            message=message
+            )
+        return redirect('result')
     return render(request, 'index/index.html')
+
+
+def result(request):
+    data = Healthdata.objects.filter(user=request.user).last()
+    return render(request, 'index/result.html', {
+        'data': data  
+    })
+
+def all_result(request):
+    data = Healthdata.objects.filter(user=request.user).all()
+    return render(request, 'index/all_result.html', {
+        'data': data  
+    })
+
 
 def create_user(request):
     return render(request, 'index/create_user.html')
+
+def usr_login(request):
+    if request.method == 'POST':
+        userid = (request.POST.get('userid')).lower()
+        try:
+            usr = User.objects.get(username=userid)
+            login(request, usr)
+            return redirect('index')
+        except:
+            pass
+        return render(request, 'index/login.html')
+
+    if request.method == 'GET':
+        return render(request, 'index/login.html')
